0f643c31fe24f53132748ed61cd7893db64e97f0
Bhishma Sontakke <bhishmasontakke15@gmail.com>
2021-09-19 11:17:10 +0530

    created makefile

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@ using namespace std;
 
 int main(){
 	cout<<"Welcome to Calculator App"<<endl;
-
+       cout<<"Select below Options"<<endl;
+      cout<<"1) Addition"<<endl<<"2) Substraction"<<endl;
+     cout<<"3)  Multiplication"<<endl<<"4) Division"<<endl;
+     cout<<"5) Quit"<<endl;
 	return 0;
 }
